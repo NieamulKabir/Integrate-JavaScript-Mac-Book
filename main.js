da218fc080381565080469e83b2ebf72b3f8f4b0
NieamulKabir <kabir000079@gmail.com>
2021-08-21 19:54:26 +0600

    added

--- main.js
+++ main.js
@@ -1,69 +1,95 @@
 // Changing Memory cost 
-const button1 = document.getElementById("button-8GB");
-button1.addEventListener("click", function() {
-    memoryCost.innerText = "0";
+const button1 = document.getElementById('button-8GB');
+button1.addEventListener('click', function() {
+    memoryCost.innerText = '0';
     totalPrice();
 });
 
-const button2 = document.getElementById("button-16GB");
-button2.addEventListener("click", function() {
-    memoryCost.innerText = "180";
+const button2 = document.getElementById('button-16GB');
+button2.addEventListener('click', function() {
+    memoryCost.innerText = '180';
     totalPrice();
 });
 
-const memoryCost = document.getElementById("memoryCost");
+const memoryCost = document.getElementById('memory_cost');
 
 
 
 // Changing Storage Cost
-const button3 = document.getElementById("button-256GB");
-button3.addEventListener("click", function() {
-    storageCost.innerText = "0";
+const button3 = document.getElementById('button-256GB');
+button3.addEventListener('click', function() {
+    storageCost.innerText = '0';
+    totalPrice();
+
+});
+
+const button4 = document.getElementById('button-512GB');
+button4.addEventListener('click', function() {
+    storageCost.innerText = '100';
     totalPrice();
 });
 
-const button4 = document.getElementById("button-512GB");
-button4.addEventListener("click", function() {
-    storageCost.innerText = "100";
+const button5 = document.getElementById('button-1TB');
+button5.addEventListener('click', function() {
+    storageCost.innerText = '180';
     totalPrice();
 });
 
-const button5 = document.getElementById("button-1TB");
-button5.addEventListener("click", function() {
-    storageCost.innerText = "180";
-    totalPrice();
-});
-
-const storageCost = document.getElementById("storageCost");
+const storageCost = document.getElementById('storage_cost');
 
 
 // Changing Delivery Cost
-const button6 = document.getElementById("deliveryFree");
-button6.addEventListener("click", function() {
-    deliveryCost.innerText = "0";
+const button6 = document.getElementById('delivery_free');
+button6.addEventListener('click', function() {
+    deliveryCost.innerText = '0';
     totalPrice();
+
 });
 
-const button7 = document.getElementById("deliveryCharge");
-button7.addEventListener("click", function() {
-    deliveryCost.innerText = "20";
+const button7 = document.getElementById('delivery_charge');
+button7.addEventListener('click', function() {
+    deliveryCost.innerText = '20';
     totalPrice();
+
 });
 
-const deliveryCost = document.getElementById("deliveryCost");
+const deliveryCost = document.getElementById('delivery_cost');
 
 
 
 // Total Price
 function totalPrice() {
-    const bestPrice1 = document.getElementById('bestPrice');
+    const bestPrice1 = document.getElementById('best_price');
     const price = bestPrice1.innerText;
     const bestprice = Number(price);
     const memoryPrice = Number(memoryCost.innerText);
     const storagePrice = Number(storageCost.innerText);
     const deliveryPrice = Number(deliveryCost.innerText);
-    const Total = bestprice + memoryPrice + storagePrice + deliveryPrice;
-    total.innerText = Total;
+    const TotalPrice = bestprice + memoryPrice + storagePrice + deliveryPrice;
+    total.innerText = TotalPrice;
+    totalAmmount.innerText = TotalPrice;
+
 }
 
-const total = document.getElementById("total");
+const total = document.getElementById('total_price');
+const totalAmmount = document.getElementById('totalrate');
+
+
+
+// promocode use 
+document.getElementById('promo_apply').addEventListener('click', function() {
+
+    const promoApply = document.getElementById('promo_apply');
+    const promoCode = document.getElementById('promo_code');
+    const applyPromo = promoCode.value;
+
+    const finalTotal = document.getElementById('totalrate');
+    const finalTotalText = finalTotal.innerText;
+
+    if (applyPromo == 'stevekaku') {
+        newTotal = finalTotalText - (finalTotalText * 20) / 100;
+        promoApply.disabled = true;
+    }
+    finalTotal.innerText = newTotal;
+    promoCode.value = '';
+})
